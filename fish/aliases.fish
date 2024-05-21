5142cd9dab698212994fa034decdd586f309cdd1
Keksi <contact@keksi.me>
2024-05-21 20:18:56 +0200

    feat(aliases): add aliases for npm and node
    Add aliases for npm and node commands:
    
    - `ngi`: `ni -g`
    - `ngun`: `nun -g`
    - `ngl`: `na list -g`
    - `nl`: `na list`

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -3,3 +3,7 @@ alias cat batcat
 alias ls "eza --icons"
 alias ll "eza --long --header --icons --git --no-permissions --no-user"
 alias la "eza --long --header --icons --git --no-permissions --no-user --all"
+alias ngi "ni -g"
+alias ngun "nun -g"
+alias ngl "na list -g"
+alias nl "na list"
