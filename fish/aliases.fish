3239ddc41c7a7565f1bba7517b1ad0f5226960f4
Keksi <contact@keksi.me>
2025-04-18 11:45:52 +0200

    fix: reorder aliases for clarity and consistency

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,12 +1,9 @@
 alias bat batcat
 alias cat batcat
-alias uvr "uv run"
 alias ls "eza --icons"
 alias ll "eza --long --header --icons --git --no-permissions --no-user"
 alias la "eza --long --header --icons --git --no-permissions --no-user --all"
-alias ngi "ni -g"
-alias ngun "nun -g"
-alias ngl "na list -g"
-alias nl "na list"
+alias uvr "uv run"
+alias nrr "nr -r"
 
 alias git "hub"
