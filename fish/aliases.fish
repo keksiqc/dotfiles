c43a57c284707ac8de3e80bd49d4b515021dc968
Keksi <contact@keksi.me>
2025-04-27 22:09:51 +0200

    fix: add missing line breaks for improved readability in aliases

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,9 +1,13 @@
 alias bat batcat
 alias cat batcat
+
 alias ls "eza --icons"
 alias ll "eza --long --header --icons --git --no-permissions --no-user"
 alias la "eza --long --header --icons --git --no-permissions --no-user --all"
+
 alias uvr "uv run"
+
 alias nrr "nr -r"
+alias ng "na -g"
 
 alias git "hub"
