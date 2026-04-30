433770ce71a40095f02932ea073535fce5c003cf
Keksi <git@keksi.dev>
2026-04-30 13:19:19 +0200

    fix: update PNPM_HOME path and ensure bin directory is added to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -108,9 +108,9 @@ if type -q atuin
 end
 
 # pnpm
-set -gx PNPM_HOME "$HOME/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+set -gx PNPM_HOME "/home/keksi/.local/share/pnpm"
+if not string match -q -- "$PNPM_HOME/bin" $PATH
+  set -gx PATH "$PNPM_HOME/bin" $PATH
 end
 # pnpm end
 
