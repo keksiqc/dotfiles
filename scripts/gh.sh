cf44d73b5bbe6b9501a603433a5c650035a5edef
Keksi <git@keksi.dev>
2026-06-29 14:23:33 +0200

    feat: add script to install and configure GitHub CLI on Debian-based systems

---
+++ scripts/gh.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# This script installs and configures the GitHub CLI (gh) on a Debian-based system.
+
+# Add keyring
+sudo mkdir -p -m 755 /etc/apt/keyrings
+out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg
+cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null
+sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
+
+# Add repository
+sudo mkdir -p -m 755 /etc/apt/sources.list.d
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+# Update package list and install GitHub CLI
+sudo apt update
+sudo apt install gh -y
+
+# Login to GitHub CLI
+gh auth login -s write:gpg_key
