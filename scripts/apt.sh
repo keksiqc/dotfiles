73721966be1822536a250f0d0de12b6ff13cafeb
Keksi <git@keksi.dev>
2026-09-01 17:49:15 +0200

    chore: simplify Fish repository addition in apt.sh

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -35,13 +35,9 @@ else
     success "GitHub CLI apt repository added."
 fi
 
-if test -f /etc/apt/sources.list.d/fish-shell-ubuntu-release-4-resolute.sources; then
-    skip "Fish repository already added."
-else
-    info "Adding Fish repository..."
-    sudo add-apt-repository ppa:fish-shell/release-4 -y
-    success "Fish repository added."
-fi
+info "Adding Fish repository..."
+sudo add-apt-repository ppa:fish-shell/release-4 -y
+success "Fish repository added."
 
 info "Updating apt packages..."
 sudo apt update -qq
