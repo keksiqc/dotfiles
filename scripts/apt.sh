9582329d661dcb841d6ab869322dc3ca5ab3985d
Keksi <git@keksi.dev>
2026-09-02 16:23:18 +0200

    chore: remove conditional checks for Mise and GitHub CLI repositories in apt.sh

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -13,28 +13,6 @@ success "extrepo installed."
 
 info "Adding apt repositories..."
 
-if test -f /etc/apt/sources.list.d/extrepo_mise.sources; then
-    skip "Mise repository already added."
-    info "Updating Mise repository..."
-    sudo extrepo update mise
-    success "Mise repository updated."
-else
-    info "Adding Mise apt repository..."
-    sudo extrepo enable mise
-    success "Mise apt repository added."
-fi
-
-if test -f /etc/apt/sources.list.d/extrepo_github-cli.sources; then
-    skip "GitHub CLI repository already added."
-    info "Updating GitHub CLI repository..."
-    sudo extrepo update github-cli
-    success "GitHub CLI repository updated."
-else
-    info "Adding GitHub CLI apt repository..."
-    sudo extrepo enable github-cli
-    success "GitHub CLI apt repository added."
-fi
-
 info "Adding Fish repository..."
 sudo add-apt-repository ppa:fish-shell/release-4 -y
 success "Fish repository added."
