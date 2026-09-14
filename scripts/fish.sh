9bf6b37847846cca9341c693fc8d5376fb0b9c7f
Keksi <git@keksi.dev>
2026-09-14 16:19:00 +0200

    chore: streamline fisher installation and plugin checks in fish.sh

--- scripts/fish.sh
+++ scripts/fish.sh
@@ -3,14 +3,18 @@ set -euo pipefail
 
 source "$(dirname "${BASH_SOURCE[0]}")/utils.sh"
 
-if test -f "$HOME/.config/fish/functions/fisher.fish"; then
+if fish -c 'type -q fisher'; then
   skip "fisher already installed."
 else
   info "Installing fisher..."
-  fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source; fisher install jorgebucaran/fisher"
+  fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source; and fisher install jorgebucaran/fisher'
   success "fisher installed."
 fi
 
-info "Installing fisher plugins..."
-fish -c "fisher install joseluisq/gitnow"
-success "fisher plugins installed."
+if fish -c 'fisher list | string match -q -- "*joseluisq/gitnow*"'; then
+  skip "fisher plugins already installed."
+else
+  info "Installing fisher plugins..."
+  fish -c 'fisher install joseluisq/gitnow'
+  success "fisher plugins installed."
+fi
