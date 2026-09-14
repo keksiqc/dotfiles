81d736e24e872257127f2966ab18329dd4fee8b3
Keksi <git@keksi.dev>
2026-09-14 16:19:58 +0200

    chore: update PNPM_HOME path to use HOME variable for portability

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -119,7 +119,7 @@ if type -q atuin
 end
 
 # pnpm
-set -gx PNPM_HOME "/home/keksi/.local/share/pnpm"
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 if not string match -q -- "$PNPM_HOME/bin" $PATH
   set -gx PATH "$PNPM_HOME/bin" $PATH
 end
