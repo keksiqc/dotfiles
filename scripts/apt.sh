117f8f7d851e073ec6480c28c4e96edcfeec0570
Keksi <git@keksi.dev>
2026-09-14 14:46:03 +0200

    chore: add check for existing Fish repository before adding

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -10,8 +10,14 @@ success "Apt packages updated."
 info "Adding apt repositories..."
 
 info "Adding Fish repository..."
-sudo add-apt-repository ppa:fish-shell/release-4 -y
-success "Fish repository added."
+if sudo grep -RqsE \
+  '^[[:space:]]*(deb(-src)?|URIs:)[[:space:]]+.*fish-shell/release-4([[:space:]]|/|$)' \
+  /etc/apt/sources.list /etc/apt/sources.list.d; then
+  skip "Fish repository already added."
+else
+  sudo add-apt-repository ppa:fish-shell/release-4 -y
+  success "Fish repository added."
+fi
 
 info "Updating apt packages..."
 sudo apt update -qq
